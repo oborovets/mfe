be659d5d495fc6111ff655a69b1dc0d2c5f20a6f
Orest <borovets.orest@gmail.com>
2023-01-31 22:09:00 +0200

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const deps = require("../package.json").dependencies;
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html"
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js"
+      },
+      shared: deps
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development")
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+function App() {
+  return (
+    <div>
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
