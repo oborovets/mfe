1f613dae6c0aa136bdb895cd2f1a4efebaca8f71
Orest Borovets <borovets.orest@gmail.com>
2023-02-02 21:42:38 +0200

    Update webpack, provide naviation between components

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,33 @@
 import React from "react";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const mount = el => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log("Navigation happens");
+      if (history.location.pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
